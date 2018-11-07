741fb3e16964f49542312ded1dbc7f869c4de619
Pasquale Caiazzo <plcaiazzo@gmail.com>
2018-11-06 21:21:09 -0600

    Add project files.

---
+++ Discord.CoD.Blops/Modules/Ping.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Discord.CoD.Blops.Modules
+{
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Hello World!");
+        }
+    }
+}
